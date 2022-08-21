b369e6cb9999c985940e97ecb9ede54c0a65c9e8
balaji <balaviji016@gmail.com>
2022-08-21 22:15:17 +0530

    6 commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,145 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './card';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const data = [
+    {
+      name: "free",
+      price: "0",
+      currency: "$",
+      period: "month",
+      features: [
+        {
+          title: "single user",
+          isEnable: true
+        },
+        {
+          title: "5GB Storage",
+          isEnable: true
+        },
+        {
+          title: "Unlimited Public Projects",
+          isEnable: true
+        },
+        {
+          title: "Community Access",
+          isEnable: true
+        },
+        {
+          title: "Unlimited Private Projects",
+          isEnable: false
+        },
+        {
+          title: "Dedicated Phone Support",
+          isEnable: false
+        },
+        {
+          title: "Free Subdomain",
+          isEnable: false
+        },
+        {
+          title: "Monthly Status Reports",
+          isEnable: false
+        },
+      ]
+    },
+       {
+        name: "plus",
+        price: "9",
+        currency: "$",
+        period: "month",
+        features: [
+          {
+            title: "5 user",
+            isEnable: true,
+            isBold: true
+          },
+          {
+            title: "50GB Storage",
+            isEnable: true
+          },
+          {
+            title: "Unlimited Public Projects",
+            isEnable: true
+          },
+          {
+            title: "Community Access",
+            isEnable: true
+          },
+          {
+            title: "Unlimited Private Projects",
+            isEnable: true
+          },
+          {
+            title: "Dedicated Phone Support",
+            isEnable: true
+          },
+          {
+            title: "Free Subdomain",
+            isEnable: true
+          },
+          {
+            title: "Monthly Status Reports",
+            isEnable: false
+          },
+        ]
+      },
+      {
+        name: "pro",
+        price: "49",
+        currency: "$",
+        period: "month",
+        features: [
+          {
+            title: "Unlimited user",
+            isEnable: true,
+            isBold: true
+          },
+          {
+            title: "150GB Storage",
+            isEnable: true
+          },
+          {
+            title: "Unlimited Public Projects",
+            isEnable: true
+          },
+          {
+            title: "Community Access",
+            isEnable: true
+          },
+          {
+            title: "Unlimited Private Projects",
+            isEnable: true
+          },
+          {
+            title: "Dedicated Phone Support",
+            isEnable: true
+          },
+          {
+            title: "Unlimited Free Subdomain",
+            isEnable: true
+          },
+          {
+            title: "Monthly Status Reports",
+            isEnable: true
+          },
+        ]
+      }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section class="pricing py-5">
+  <div class="container">
+    <div class="row">
+     {
+      data.map((item)=>{
+        return <Card item={item}></Card>
+      })
+     }
     </div>
+    </div>
+  </section>
   );
 }
 
